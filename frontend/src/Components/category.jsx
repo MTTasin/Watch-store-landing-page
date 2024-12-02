2cf5248419eb89c9fa69e70c57ebb37d8ef3c1b5
M.T. Tasin <m.t.tasin20@gmail.com>
2024-12-02 16:23:29 +0600

    fixed a little bug

--- frontend/src/Components/category.jsx
+++ frontend/src/Components/category.jsx
@@ -7,9 +7,9 @@ export default function Category(){
     return(
         <>
             <div className="grid md:grid-cols-3 grid-cols-1 gap-4 mt-10">
-                <div className="md:border-r-8 border-b-8 border-[#cca471] "><img src={men} alt="" className="p-5" /></div>
-                <div className="md:border-x-8 border-y-8 border-[#cca471]"><img src={couple} alt="" className="p-5" /></div>
-                <div className="md:border-l-8 border-t-8 border-[#cca471]"><img src={women} alt="" className="p-5" /></div>
+                <div className="md:border-r-8 md:border-b-0 border-b-8 border-[#cca471] "><img src={men} alt="" className="p-5" /></div>
+                <div className="md:border-x-8 md:border-y-0 border-y-8 border-[#cca471]"><img src={couple} alt="" className="p-5" /></div>
+                <div className="md:border-l-8 md:border-t-0 border-t-8 border-[#cca471]"><img src={women} alt="" className="p-5" /></div>
             </div>
         </>
     )
