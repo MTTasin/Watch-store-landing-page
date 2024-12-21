3e230fd766d6a1c889f173d0bf445452b8087ba7
M.T. Tasin <m.t.tasin20@gmail.com>
2024-12-21 18:05:07 +0600

    Made the card component dynamic

--- frontend/src/Components/Card.jsx
+++ frontend/src/Components/Card.jsx
@@ -1,8 +1,7 @@
-import w1 from "./../assets/img/w1.png";
 import cardbg from "./../assets/cardbg.jpg";
 import StarReview from "./StarReview";
 
-export default function Card() {
+export default function Card(props) {
 
 
 
@@ -19,16 +18,16 @@ export default function Card() {
               />
             </div>
             <div className="absolute z-10 w-[80%] h-[90%] top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-                <img src={w1} alt="" className="" />
+                <img src={props.image} alt="" className="" />
             </div>
             <div className="relative p-6 pt-32 text-center bg-black">
               <h4 className="block font-sans text-2xl antialiased font-semibold leading-snug tracking-normal fertuzy">
-                Armani
+                {props.title}
               </h4>
               <p className="block mt-3 font-sans text-xl antialiased leading-relaxed norm2nd">
-                $99.99
+                ${props.price}
               </p>
-              <div className="mb-2 flex justify-center"><StarReview rating={4.5} /><span className="ml-2 mt-4">4.5</span></div>
+              <div className="mb-2 flex justify-center"><StarReview rating={props.rating} /><span className="ml-2 mt-4">{props.rating}</span></div>
               <div className="mb-10 flex justify-center"><p>In stock</p></div>
               <button className="btn z-20 bg-[#343435]  w-1/2 h-12 border-b-8 active:border-b-4 border-[#d4ab84] rounded-lg absolute bottom-0 left-1/2 transform -translate-x-1/2">Add to cart</button>
             </div>
--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -2,6 +2,10 @@ import NavBar from "../Components/NavBar";
 import Hero from "../Components/Hero";
 import Category from "../Components/category";
 import Card from "../Components/Card";
+import w1 from "./../assets/img/w1.png";
+import w2 from "./../assets/img/w2.png";
+import w3 from "./../assets/img/w3.png";
+import w4 from "./../assets/img/w4.png";
 
 export default function Home() {
     return (
@@ -12,10 +16,30 @@ export default function Home() {
 
             <div><h2 className="text-center mt-10 text-3xl font-bold text-white underline armani fertuzy">Best In Store</h2></div>
             <div className="flex flex-wrap gap-2 justify-center">
-                <Card />
-                <Card />
-                <Card />
-                <Card />
+                <Card
+                    image={w1}
+                    title="Armani Blackstone"
+                    price="400"
+                    rating={4}
+                />
+                <Card
+                    image={w2}
+                    title="Casio G-Shock"
+                    price="900"
+                    rating={4.9}
+                />
+                <Card
+                    image={w3}
+                    title="Rolex"
+                    price="1500"
+                    rating={5}
+                />
+                <Card 
+                    image={w4}
+                    title="Titan"
+                    price="1000"
+                    rating={3.4}
+                />
             </div>
             
         </>
